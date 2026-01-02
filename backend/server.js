2c05d24dd4e2f3dd43a85a5dd7fa587dae19210c
aasthanuix <digital.aasthanuix@gmail.com>
2026-01-02 13:18:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ io.on('connection', (socket) => {
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://universalsexports.com",
+  "https://www.universalsexports.com",
   "https://aastha-chits-react-2025.vercel.app",
 ];
 
