6f48f10958f997571919655e12f5632e40833787
aasthanuix <digital.aasthanuix@gmail.com>
2026-01-08 17:08:25 +0530

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,1 +1,9 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:4000/api",
+  withCredentials: true,
+});
+
+export const fetchChitPlans = () => API.get("/chit-plans");
 
